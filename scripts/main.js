acb9fb7e38473e0cd1f32ef20d93a5fd543e8fb0
bayoishola20 <bayoishola20@yahoo.com>
2019-11-16 10:15:10 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,10 @@ $(window).load(function(){
     $('#welcomeModal').modal('show');
   }); 
   // ======== END: On screen load howToModal
+
+// ======= Lightbox toggle
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+// ========
